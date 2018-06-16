e47aa68a3844b9776ca4c88ae40b994aa932c01c
Daren Lunsford <dlunsfo1@asu.edu>
2018-06-16 18:34:19 -0500

    Updates db env names

--- server/server.js
+++ server/server.js
@@ -4,14 +4,12 @@ var api = require('./api/api');
 var config = require('./config/config');
 
 //require('mongoose').connect('database:27017/dlunsfordServiceDev'); // used for docker compose
-require('mongoose').connect(
-  'mongodb+srv://thesis2018:PaPcjLhw5gji1v00@cluster0-iz4z3.mongodb.net/dlunsfordService'
-);
+require('mongoose').connect(process.env.DB_MLAB);
 
 // require('mongoose').connect(
 //   'mongodb://mongo/dlunsfordServiceDocker',
 //   { poolSize: 200 }
-// );
+// )
 
 //setup middlware from a single point
 require('./middleware/middleware')(app);
