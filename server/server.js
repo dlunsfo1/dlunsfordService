9cafbf6c4eea99bea8068d91186ec9134f6b3092
Daren Lunsford <dlunsfo1@asu.edu>
2018-06-16 18:12:56 -0500

    ...

--- server/server.js
+++ server/server.js
@@ -4,7 +4,9 @@ var api = require('./api/api');
 var config = require('./config/config');
 
 //require('mongoose').connect('database:27017/dlunsfordServiceDev'); // used for docker compose
-require('mongoose').connect(process.env.DB_URI);
+require('mongoose').connect(
+  'mongodb+srv://thesis2018:PaPcjLhw5gji1v00@cluster0-iz4z3.mongodb.net/dlunsfordService'
+);
 
 // require('mongoose').connect(
 //   'mongodb://mongo/dlunsfordServiceDocker',
